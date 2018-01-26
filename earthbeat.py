8c64395ada6eb33528a4174477b8d4cbde24dce6
combabr <bruno.combal@ec.europa.eu>
2018-01-26 10:50:23 +0100

    implementing click

--- earthbeat.py
+++ earthbeat.py
@@ -27,12 +27,11 @@ import resources
 
 # Import the code for the DockWidget
 from earthbeat_dockwidget import EarthBeatDockWidget
+import earthbeatUtil
 import os.path
-
 
 class EarthBeat:
     """QGIS Plugin Implementation."""
-
     def __init__(self, iface):
         """Constructor.
 
@@ -101,44 +100,6 @@ class EarthBeat:
         status_tip=None,
         whats_this=None,
         parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
 
         icon = QIcon(icon_path)
         action = QAction(icon, text, parent)
@@ -208,6 +169,8 @@ class EarthBeat:
 
     #--------------------------------------------------------------------------
 
+    #--------------------------------------------------------------------------
+
     def run(self):
         """Run method that loads and starts the plugin"""
 
@@ -222,6 +185,8 @@ class EarthBeat:
             if self.dockwidget == None:
                 # Create the dockwidget (after translation) and keep reference
                 self.dockwidget = EarthBeatDockWidget()
+                self.canvas_clicked = earthbeatUtil.getClickedPoint( self.iface.mapCanvas(), self.dockwidget )
+                self.iface.mapCanvas().setMapTool( self.canvas_clicked )
 
             # connect to provide cleanup on closing of dockwidget
             self.dockwidget.closingPlugin.connect(self.onClosePlugin)
